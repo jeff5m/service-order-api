206fb14c22a47bfb2c600c11cdb9e660eb7d2578
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-16 21:21:20 -0300

    remove validations from Service Order entity

--- src/main/java/com/soservice/domain/model/ServiceOrder.java
+++ src/main/java/com/soservice/domain/model/ServiceOrder.java
@@ -1,17 +1,11 @@
 package com.soservice.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.soservice.domain.ValidationGroups;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
@@ -25,24 +19,14 @@ public class ServiceOrder {
     private Long id;
 
     @Valid
-    @ConvertGroup(from = Default.class, to = ValidationGroups.ClientId.class)
-    @NotNull
     @ManyToOne
     private Client client;
 
-    @NotBlank
     private String description;
-
-    @NotNull
     private BigDecimal price;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private ServiceOrderStatus status;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private OffsetDateTime createdAt;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private OffsetDateTime finishedAt;
 }
