6443a11a32d8d610912660eed7c66c59965587e1
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-18 19:57:45 -0300

    create Comment repository

---
+++ src/main/java/com/soservice/domain/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package com.soservice.domain.repository;
+
+import com.soservice.domain.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
